865826c21c159296598c5f54d487982a7fb3c6dd
F3rr3ir4 <leandroferreira757@msn.com>
2020-02-04 15:54:53 -0300

    Create example axios

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-alert("opa");
+import axios from "axios";
+
+class Api {
+    static async getUserInfo(username) {
+        try {
+            const response = await axios.get(
+                `https://api.github.com/users/${username}`
+            );
+            console.log(response);
+        } catch (err) {
+            console.log("Erro na API");
+        }
+    }
+}
+
+Api.getUserInfo("F3rr3ir4");
